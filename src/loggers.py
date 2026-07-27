bed1370d966e0f2b5cda4098dc96b31bced63012
Satti Bhavyasri <bhavyareddysatti6@gmail.com>
2026-07-27 19:30:24 +0530

    Update loggers.py

--- src/loggers.py
+++ src/loggers.py
@@ -1,31 +1,18 @@
 import pandas as pd
 
 
-def save_phase1_log(log_data, log_file_path):
+def save_log(log, file_path):
     """
-    Saves the Phase 1 execution log to an Excel file.
+    Save logs to Excel.
     """
 
-    log_df = pd.DataFrame(
-        log_data,
-        columns=["Contact", "Status", "Time"]
+    df = pd.DataFrame(
+        log,
+        columns=[
+            "Contact",
+            "Status",
+            "Time"
+        ]
     )
 
-    log_df.to_excel(log_file_path, index=False)
-
-    return log_df
-
-
-def save_phase2_log(retry_log_data, retry_log_file_path):
-    """
-    Saves the Phase 2 retry log to an Excel file.
-    """
-
-    retry_log_df = pd.DataFrame(
-        retry_log_data,
-        columns=["Contact", "Status", "Time"]
-    )
-
-    retry_log_df.to_excel(retry_log_file_path, index=False)
-
-    return retry_log_df
+    df.to_excel(file_path, index=False)
